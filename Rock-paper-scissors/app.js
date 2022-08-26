4f9bf435b67509b99a543e0271da5490cb92aee4
Luo <lostxi@hotmail.com>
2022-08-26 02:25:13 +0100

    computer choice

--- Rock-paper-scissors/app.js
+++ Rock-paper-scissors/app.js
@@ -1,20 +1,40 @@
-const computerChoice = document.getElementById("computer-choice");
+const computerChoiceDisplay = document.getElementById("computer-choice");
 const userChoiceDisplay = document.getElementById("your-choice");
 const resultDisplay = document.getElementById("result");
 const possibleChoices = document.querySelectorAll(".btn");
 let userChoice;
-
-/* possibleChoices.forEach((possibleChoice) =>
-  possibleChoice.addEventListener("click", (e) => {
-    userChoice = e.target.id;
-    userChoiceDisplay.innerHTML = userChoice;
-  })
-);
- */
+let computerChoice;
 
 possibleChoices.forEach((possibleChoice) =>
   possibleChoice.addEventListener("click", (e) => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
   })
 );
+
+function generateComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1;
+  if (randomNumber === 1) {
+    computerChoice = "Rock";
+  } else if (randomNumber === 2) {
+    computerChoice = "Paper";
+  } else if (randomNumber === 3) {
+    computerChoice = "Scissors";
+  }
+  computerChoiceDisplay.innerHTML = computerChoice;
+}
+/* possibleChoices.forEach((possibleChoice) =>
+  possibleChoice.addEventListener("click", () => {
+    let randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1;
+    if (randomNumber === 1) {
+      computerChoice = "Rock";
+    } else if (randomNumber === 2) {
+      computerChoice = "Paper";
+    } else if (randomNumber === 3) {
+      computerChoice = "Scissors";
+    }
+    computerChoiceDisplay.innerHTML = computerChoice;
+  })
+);
+ */
