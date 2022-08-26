d1394ece15fa86b84548db3d27de525640d45e3d
Luo <lostxi@hotmail.com>
2022-08-26 02:47:54 +0100

    result

--- Rock-paper-scissors/app.js
+++ Rock-paper-scissors/app.js
@@ -4,12 +4,13 @@ const resultDisplay = document.getElementById("result");
 const possibleChoices = document.querySelectorAll(".btn");
 let userChoice;
 let computerChoice;
-
+let result;
 possibleChoices.forEach((possibleChoice) =>
   possibleChoice.addEventListener("click", (e) => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
     generateComputerChoice();
+    getResult();
   })
 );
 
@@ -38,3 +39,22 @@ function generateComputerChoice() {
   })
 );
  */
+
+function getResult() {
+  if (userChoice === "Rock" && computerChoice === "Scissors") {
+    result = "You won!";
+  } else if (userChoice === "Scissors" && computerChoice === "Paper") {
+    result = "You won!";
+  } else if (userChoice === "Paper" && computerChoice === "Rock") {
+    result = "You won!";
+  } else if (userChoice === "Paper" && computerChoice === "Scissors") {
+    result = "You lost!";
+  } else if (userChoice === "Scissors" && computerChoice === "Rock") {
+    result = "You lost!";
+  } else if (userChoice === "Rock" && computerChoice === "Paper") {
+    result = "You lost!";
+  } else if (userChoice === computerChoice) {
+    result = "It is a draw!";
+  }
+  resultDisplay.innerHTML = result;
+}
