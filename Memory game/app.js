c2c2a5b5045ea3e18c3d4b4f418fbbc9254df121
Luo <lostxi@hotmail.com>
2022-08-27 05:18:05 +0100

    score

--- Memory game/app.js
+++ Memory game/app.js
@@ -1,21 +1,26 @@
 const cardArray = [
-  { imgName: "leaf1", imgAdress: "./images/leaf1" },
-  { imgName: "leaf2", imgAdress: "./images/leaf2" },
-  { imgName: "leaf3", imgAdress: "./images/leaf3" },
-  { imgName: "flower1", imgAdress: "./images/flower1" },
-  { imgName: "flower2", imgAdress: "./images/flower2" },
-  { imgName: "flower3", imgAdress: "./images/flower3" },
-  { imgName: "leaf1", imgAdress: "./images/leaf1" },
-  { imgName: "leaf2", imgAdress: "./images/leaf2" },
-  { imgName: "leaf3", imgAdress: "./images/leaf3" },
-  { imgName: "flower1", imgAdress: "./images/flower1" },
-  { imgName: "flower2", imgAdress: "./images/flower2" },
-  { imgName: "flower3", imgAdress: "./images/flower3" },
+  { imgName: "leaf1.png", imgAdress: "./images/leaf1.png" },
+  { imgName: "leaf2.png", imgAdress: "./images/leaf2.png" },
+  { imgName: "leaf3.png", imgAdress: "./images/leaf3.png" },
+  { imgName: "flower1.png", imgAdress: "./images/flower1.png" },
+  { imgName: "flower2.png", imgAdress: "./images/flower2.png" },
+  { imgName: "flower3.png", imgAdress: "./images/flower3.png" },
+  { imgName: "leaf1.png", imgAdress: "./images/leaf1.png" },
+  { imgName: "leaf2.png", imgAdress: "./images/leaf2.png" },
+  { imgName: "leaf3.png", imgAdress: "./images/leaf3.png" },
+  { imgName: "flower1.png", imgAdress: "./images/flower1.png" },
+  { imgName: "flower2.png", imgAdress: "./images/flower2.png" },
+  { imgName: "flower3.png", imgAdress: "./images/flower3.png" },
 ];
 
 cardArray.sort(() => 0.5 - Math.random());
 
 const gridDisplay = document.querySelector("#grid");
+let score = document.getElementById("result");
+let num = 0;
+score.innerHTML = num;
+let cardName = [];
+let cardChosenId = [];
 
 function createBoard() {
   for (let i = 0; i < cardArray.length; i++) {
@@ -30,19 +35,57 @@ function createBoard() {
 
 createBoard();
 
-/* function flipCard() {
-  for (let i = 0; i < cardArray.length; i++) {
-    const eachCard = document.getElementById(i);
-    eachCard.addEventListener("click", () => {
-      const cardId = eachCard.getAttribute("id");
-      console.log("clicked", cardId);
-    });
-  }
-}
-flipCard();
- */
-
 function flipCard() {
   const cardId = this.getAttribute("data-id");
-  console.log("clicked", cardId);
+  const imgName = cardArray[cardId].imgName;
+  const imgAdress = cardArray[cardId].imgAdress;
+
+  this.setAttribute("src", imgAdress);
+
+  cardName.push(imgName);
+  cardChosenId.push(cardId);
+
+  if (cardName.length === 2) {
+    setTimeout(() => {
+      findSameCards();
+    }, 200);
+  } else if (cardName.length > 2) {
+    setTimeout(() => {
+      resetBoard();
+    }, 200);
+  }
+}
+
+function findSameCards() {
+  const allImgs = document.querySelectorAll("#grid img");
+  const imgIndex1 = cardChosenId[0];
+  const imgIndex2 = cardChosenId[1];
+  if (imgIndex1 === imgIndex2) {
+    alert("You have clicked the same image!");
+    allImgs[imgIndex1].setAttribute("src", "./images/blank.png");
+    cardName = [];
+    cardChosenId = [];
+  } else if (cardName[0] === cardName[1]) {
+    allImgs[imgIndex1].setAttribute("src", "./images/white.png");
+    allImgs[imgIndex2].setAttribute("src", "./images/white.png");
+    allImgs[imgIndex1].removeEventListener("click", flipCard);
+    allImgs[imgIndex2].removeEventListener("click", flipCard);
+    num += 1;
+    score.innerHTML = num;
+    cardName = [];
+    cardChosenId = [];
+  } else {
+    resetBoard();
+  }
+}
+
+function resetBoard() {
+  const imgIndex1 = cardChosenId[0];
+  const imgIndex2 = cardChosenId[1];
+  const allImgs = document.querySelectorAll("#grid img");
+  allImgs[imgIndex1].setAttribute("src", "./images/blank.png");
+  allImgs[imgIndex2].setAttribute("src", "./images/blank.png");
+
+  cardChosenId = [];
+  cardName = [];
 }
