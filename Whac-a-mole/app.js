426ce541e203b0de10ba9d2bbdf7b26d85db6e49
Luo <lostxi@hotmail.com>
2022-08-31 03:43:04 +0100

    hit position

--- Whac-a-mole/app.js
+++ Whac-a-mole/app.js
@@ -4,13 +4,27 @@ const score = document.querySelector("#score");
 const timeLeft = document.querySelector("#time-left");
 const btnStart = document.querySelector("#start-btn");
 const btnEnd = document.querySelector("#end-btn");
+let hitPosition;
+let result = 0;
 function randomSquare() {
   squares.forEach((square) => {
     square.classList.remove("mole");
   });
   const randomSqr = squares[Math.floor(Math.random() * 9)];
   randomSqr.classList.add("mole");
+  hitPosition = randomSqr.id;
 }
+
+squares.forEach((square) =>
+  square.addEventListener("mousedown", () => {
+    if (hitPosition === square.id) {
+      result += 1;
+    }
+    score.innerHTML = result;
+    hitPosition = null;
+  })
+);
+
 let timeId;
 function moveMole() {
   timeId = null;
@@ -25,4 +39,3 @@ function endMoveMole() {
 
 btnStart.addEventListener("click", moveMole);
 btnEnd.addEventListener("click", endMoveMole);
-//
