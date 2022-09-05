e801cb5554bcbb2c9014d93ea85b754ea1243fd2
Luo <lostxi@hotmail.com>
2022-09-05 04:14:56 +0100

    key event

--- Frogger/index.js
+++ Frogger/index.js
@@ -31,9 +31,25 @@ function setDivAttribute() {
 setDivAttribute();
 
 const squares = document.querySelectorAll(".grid div");
-let currentIndex = 0;
-console.log(squares);
-function moveFrog() {
+let currentIndex = 76;
+
+function moveFrog(event) {
+  switch (event.key) {
+    case "ArrowLeft":
+      console.log("left");
+      break;
+    case "ArrowRight":
+      console.log("right");
+      break;
+    case "ArrowUp":
+      console.log("up");
+      break;
+    case "ArrowDown":
+      console.log("down");
+      break;
+    default:
+      break;
+  }
   squares[currentIndex].classList.add("frog");
 }
 
