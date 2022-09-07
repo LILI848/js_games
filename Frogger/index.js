1b360757fa6f943e3e40fd531f8af4c6656fa3f8
Luo <lostxi@hotmail.com>
2022-09-07 03:53:24 +0100

    change class names and apply css(l1-l5)

--- Frogger/index.js
+++ Frogger/index.js
@@ -4,13 +4,15 @@ const timeLeftDisplay = document.querySelector("#time-left");
 const resultDisplay = document.querySelector("#result");
 const startPauseButton = document.querySelector("#start-pause-button");
 
+let divArr = [];
+
 for (let i = 0; i <= 80; i++) {
   const divDisplay = document.createElement("div");
   gridDisplay.append(divDisplay);
 }
 
 function setDivAttribute() {
-  const divs = document.querySelectorAll("div");
+  let divs = document.querySelectorAll("div");
   divs[5].setAttribute("class", "ending-block");
   divs[77].setAttribute("class", "starting-block");
   for (let i = 19; i <= 27; i++) {
@@ -26,9 +28,53 @@ function setDivAttribute() {
   for (let i = 55; i <= 63; i++) {
     divs[i].setAttribute("class", "car-right");
   }
+  for (let i = 19; i <= 36; i++) {
+    divArr.push(divs[i]);
+  }
 }
 
 setDivAttribute();
+
+function groupArr(data, num) {
+  let group = [];
+
+  for (let i = 0, j = 0; i < data.length; i++) {
+    if (i >= num && i % num === 0) {
+      j++;
+    }
+    group[j] = group[j] || [];
+    group[j].push(data[i]);
+  }
+  return group;
+}
+let groupDivs = groupArr(divArr, 5);
+
+console.log(divArr);
+console.log(groupDivs);
+
+console.log(divArr[1]);
+
+function addClass(arr) {
+  let newDiv = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    newDiv = arr[i].map((div) => {
+      let indexOfDiv = arr[i].indexOf(div) + 1;
+      console.log(indexOfDiv);
+      div.classList.add(`l${indexOfDiv}`);
+
+      return div;
+    });
+
+    console.log(newDiv);
+  }
+  return newDiv;
+}
+
+let addClassDivs = addClass(groupDivs);
+console.log(addClassDivs);
+
+console.log(document.querySelectorAll(".l1"));
 
 const squares = document.querySelectorAll(".grid div");
 let currentIndex = 76;
