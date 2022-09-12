e309f77c418c33c267aa98ecd91ebdcfc2748457
Luo <lostxi@hotmail.com>
2022-09-12 22:18:04 +0100

    create elements

--- Connect Four/index.js
+++ Connect Four/index.js
@@ -1,4 +1,5 @@
 const grid = document.querySelector(".grid");
+
 for (let i = 0; i < 48; i++) {
   const div = document.createElement("div");
   grid.append(div);
@@ -8,4 +9,6 @@ let divs = document.querySelectorAll("div");
 for (let j = 42; j <= 48; j++) {
   divs[j].setAttribute("class", "taken");
 }
-//
+const squares = document.querySelectorAll(".grid div");
+const result = document.querySelector("#result");
+const displayCurrentPlayer = document.querySelector("#current-player");
